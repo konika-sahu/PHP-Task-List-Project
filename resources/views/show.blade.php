57ec74a5025d262b280438fe3e7333094cb3ea1d
konika-sahu <140963226+konika-sahu@users.noreply.github.com>
2024-07-15 08:25:46 +0530

    modify show blade template with blade directives such as yield, extends and section directives

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,6 +1,8 @@
-<h1>
-    {{$task -> title}}
-</h1>
+@extends('layouts.app')
+
+@section('title', $task -> title)
+
+@section('content')
 
 <p>{{$task -> description}} </p>
 
@@ -10,3 +12,4 @@
 
 <p>{{$task -> created_at}} </p>
 <p>{{$task -> updated_at}} </p>
+@endsection
