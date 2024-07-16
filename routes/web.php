b8f8474dc55b3fd712d841a15ba675fc623abf23
konika-sahu <140963226+konika-sahu@users.noreply.github.com>
2024-07-16 08:52:43 +0530

    modify web.php to add route name task.update

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,17 @@ Route::get('/', function(){
 
 Route::get('/tasks', function() {
     return view('index', [
-        'tasks' => Task::latest()->where('completed', true)->get()
+        'tasks' => Task::latest()->where('completed', false)->get()
     ]);
 })-> name('tasks.index');
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
+
+Route::get('/tasks/{id}/edit', function($id) {
+    return view('edit', [
+      'task'=> Task::findOrFail($id)
+    ]);
+})-> name('tasks.show');
 
 Route::get('/tasks/{id}', function($id) {
     return view('show', [
@@ -37,8 +43,28 @@ Route::post('/tasks', function(Request $request){
   $task->long_description = $data['long_description'];
   $task->save();
 
-  return redirect()->route('tasks.show', ['id' => $task->id]);
+  return redirect()->route('tasks.show', ['id' => $task->id])
+  ->with('success', 'Task created successfully!');
+
 })->name('tasks.store');
+
+Route::put('/tasks/{id}', function($id, Request $request){
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+  
+    $task = Task::findOrFail($id);
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+  
+    return redirect()->route('tasks.show', ['id' => $task->id])
+    ->with('success', 'Task updated successfully!');
+  
+  })->name('tasks.update');
 
 Route::fallback( function(){
     return 'Still got somewhere!';
