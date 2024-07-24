cd7c23693a5199df198882aaa1bc8d877e02f841
konika-sahu <140963226+konika-sahu@users.noreply.github.com>
2024-07-25 01:04:00 +0530

    modify web-php for using form request i.e. using same validation rules extracted to another place without repeating them over and over again.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -30,37 +31,21 @@ Route::get('/tasks/{task}', function(Task $task) {
     ]);
 })-> name('tasks.show');
 
-Route::post('/tasks', function(Request $request){
-  $data = $request->validate([
-      'title' => 'required|max:255',
-      'description' => 'required',
-      'long_description' => 'required'
-  ]);
+Route::post('/tasks', function(TaskRequest $request){
 
-  $task = new Task;
-  $task->title = $data['title'];
-  $task->description = $data['description'];
-  $task->long_description = $data['long_description'];
-  $task->save();
+    $task = Task::create($request->validated());
 
-  return redirect()->route('tasks.show', ['id' => $task->id])
+  return redirect()->route('tasks.show', ['task' => $task->id])
   ->with('success', 'Task created successfully!');
 
 })->name('tasks.store');
 
-Route::put('/tasks/{task}', function(Task $task, Request $request){
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required'
-    ]);
+Route::put('/tasks/{task}', function(Task $task, TaskRequest $request){
+
+    $task->update($request->validated());
+
   
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
-  
-    return redirect()->route('tasks.show', ['id' => $task->id])
+    return redirect()->route('tasks.show', ['task' => $task->id])
     ->with('success', 'Task updated successfully!');
   
   })->name('tasks.update');
