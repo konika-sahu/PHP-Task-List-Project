9d57aa587cdb921562ffb2e405435cab77c5ef1f
konika-sahu <140963226+konika-sahu@users.noreply.github.com>
2024-07-26 17:51:38 +0530

    modify index blade template, add a link to redirect to the Add Task page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,10 @@
 @section('title', 'The list of tasks')
 
 @section('content')
-    {{-- @if (count($tasks)) --}}
+    <div>
+        <a href="{{ route('tasks.create')}}"> Add Task! </a>
+    </div>
+
     @forelse ($tasks as $task)
             <div> 
                 <a href=" {{ route('tasks.show', ['task'=> $task->id ]) }}"> {{$task -> title }} </a>
@@ -11,7 +14,7 @@
     @empty
         <div>Oops! There are no Tasks!</div>
     @endforelse
-    {{-- @endif --}}
+    
 
     @if($tasks->count())
         <nav>
