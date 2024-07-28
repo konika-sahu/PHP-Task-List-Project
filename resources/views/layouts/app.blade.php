da7b961cdd51fc53fa6669247a93c14a50da94b8
konika-sahu <140963226+konika-sahu@users.noreply.github.com>
2024-07-28 12:18:29 +0530

    modify app layout view, add tailwindcss style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,38 @@
 
     <head>
         <title>Laravel 11 Task List App </title>
+        <script src="https://cdn.tailwindcss.com"></script>
+
+        {{--blade-formatter-disable--}}
+        <style type="text/tailwindcss">
+            .btn {
+                @apply rounded-md px-2 py-1 text-center font-medium shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-100 text-slate-700
+            }
+
+            .link{
+                @apply font-medium text-gray-700 underline decoration-pink-500
+            }
+
+            label{
+                @apply block uppercase text-slate-700 mb-4
+            }
+
+            input, textarea{
+                @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none
+            }
+
+            .error{
+                @apply text-red-500 text-sm
+            }
+
+        </style>
+        {{--blade-formatter-enable--}}
+
         @yield('styles')
     </head>
 
-    <body>
-        <h1>@yield('title')</h1>
+    <body class= "container mx-auto mt-10 mb-10 max-w-lg">
+        <h1 class="mb-4 text-2xl">@yield('title')</h1>
 
         <div>
             @if(session()->has('success'))
